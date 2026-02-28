2447f29dcea42b69d601ef2518302b5ab2034640
José Segura <jose.segura.ms@gmail.com>
2026-02-28 10:45:14 -0300

    Creating project and git repository

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+import db.DbException;
+
+public class Program {
+	public static void main(String[] args) {
+	}
+}
